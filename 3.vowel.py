86655da8b7c12b3bb9573f5d58d4a76766b52a6a
Jesima8 <52022576+Jesima8@users.noreply.github.com>
2019-06-20 21:09:33 +0530

    Create 3.vowel.py

---
+++ 3.vowel.py
@@ -0,0 +1,7 @@
+b=input("")
+if(b=='A' or b=='a' or b=='E'or b=='e' or b=='I' or b=='i' or b=='O'or b=='o' or b=='U'or b=='u' ):
+    print("Vowel")
+elif(b=='$'):
+    print("invalid")
+else:
+    print("Constant")
